de5aea64d7c9dd440a4afc5b7fc8e5c2fdef9a04
Wahid Alimi <wahid.alimi@gmail.com>
2025-11-04 12:41:52 -0500

    feat: Complete business card content analysis: Reads the contents of the image file Sets the version of the Content Understanding REST API to be used Submits an HTTP POST request to your Content Understanding endpoint, instructing the to analyze the image. Checks the response from the operation to retrieve an ID for the analysis operation. Repeatedly submits an HTTP GET request to your Content Understanding endpoint to check the operation status until it is no longer running. If the operation has succeeded, saves the JSON response, and then parses the JSON and displays the values retrieved for each type-specific field.

--- Labfiles/content-app/read-card.py
+++ Labfiles/content-app/read-card.py
@@ -37,6 +37,72 @@ def main():
 def analyze_card (image_file, analyzer, endpoint, key):
     
     # Use Content Understanding to analyze the image
+    print(f"Analyzing {image_file}")
+
+    # Set the API version
+    CU_VERSION = "2025-05-01-preview"
+
+    # Read the image data
+    with open(image_file, "rb") as file:
+        image_data = file.read()
+
+    ## Use a POST request to submit the image data to the analyzer
+    print("Submitting request...")
+    headers = {
+        "Ocp-Apim-Subscription-Key": key,
+        "Content-Type": "application/octet-stream"
+    }
+    url = f'{endpoint}/contentunderstanding/analyzers/{analyzer}:analyze?api-version={CU_VERSION}'
+    response = requests.post(url, headers=headers, data=image_data)
+
+    # Get the response and extract the ID assigned to the analysis operation
+    print(response.status_code)
+    response_json = response.json()
+    id_value = response_json.get("id")
+
+    # Use a GET request to check the status of the analysis operation
+    print('Getting results....')
+    time.sleep(1)
+    result_url = f'{endpoint}/contentunderstanding/analyzerResults/{id_value}?api-version={CU_VERSION}'
+    result_response = requests.get(result_url, headers=headers)
+    print(result_response.status_code)
+
+    # Keep polling until the analysis is complete
+    status = result_response.json().get("status")
+    while status == "Running":
+        time.sleep(1)
+        result_response = requests.get(result_url, headers=headers)
+        status = result_response.json().get("status")
+
+    # Process the analysis results
+    if status == "Succeeded":
+        print("Analysis succeeded:\n")
+        result_json = result_response.json()
+        output_file = "results.json"
+        with open(output_file, "w") as json_file:
+            json.dump(result_json, json_file, indent=4)
+            print(f"Response saved in {output_file}\n")
+
+
+        # Iterate through the fields and extract the names and type-specific values
+        contents = result_json["result"]["contents"]
+        for content in contents:
+            if "fields" in content:
+                fields = content["fields"]
+                for field_name, field_data in fields.items():
+                    if field_data['type'] == "string":
+                        print(f"{field_name}: {field_data['valueString']}")
+                    elif field_data ['type'] == "number":
+                        print(f"{field_name}: {field_data['valueNumber']}")
+                    elif field_data['type'] == "integer":
+                        print(f"{field_name}: {field_data['valueInteger']}")
+                    elif field_data['type'] == "date":
+                        print(f"{field_name}: {field_data['valueDate']}")
+                    elif field_data['type'] == "time":
+                        print(f"{field_name}: {field_data['valueTime']}")
+                    elif field_data['type'] == "array":
+                        print(f"{field_name}: {field_data['valueArray']}")
+
 
 
 
